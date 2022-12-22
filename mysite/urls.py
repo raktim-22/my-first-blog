8b533f4788e7c226994e1694f8eb4c6e026e1d60
raktim <rmajumdar987@gmail.com>
2022-12-22 15:25:03 +0530

    modified templates to disp posts from database

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls')),
+    path('',include('blog.urls')), #django redirects to blogs.url and looks for further commands
+    
 ]
