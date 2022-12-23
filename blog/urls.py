331c11c3604054b72a243dbfb6df9a0e7aa1ad04
raktim <rmajumdar987@gmail.com>
2022-12-23 16:15:05 +0530

    added view, detailed view,page not found and css template

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #That means if you enter http://127.0.0.1:8000/post/5/ into your browser, Django will understand that you are looking for a view called post_detail and transfer the information that pk equals 5 to that view
 ]
