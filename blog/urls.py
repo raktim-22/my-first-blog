21364321ef7a0decbad2896bf0a20b740faa190d
raktim <rmajumdar987@gmail.com>
2022-12-26 15:59:39 +0530

    added login, drafts, and publisher pages

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     #That means if you enter http://127.0.0.1:8000/post/5/ into your browser, Django will understand that you are looking for a view called post_detail and transfer the information that pk equals 5 to that view
 ]
